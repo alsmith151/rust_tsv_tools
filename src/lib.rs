130d0215ad347488fa25d36c077f22cc38782960
asmith <asmith151@outlook.com>
2022-02-11 13:55:19 +0000

    Added split_by_fragment_size to lib

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ pub mod split_tabix;
 
 // Splits a tabix files into separate files using supplied barcodes
 #[pyfunction]
+#[pyo3(text_signature = "(tabix, barcodes, /)")]
 #[pyo3(name = "split_tabix_by_barcode")]
-fn split_tabix_py(
+fn split_tabix_by_barcode_py(
     tabix: String,
     barcodes: HashMap<String, HashSet<String>>,
     _n_threads: Option<u8>,
@@ -22,8 +23,28 @@ fn split_tabix_py(
 
 }
 
+// Splits a tabix files into separate files by fragment length
+#[pyfunction]
+#[pyo3(text_signature = "(tabix, bins, /)")]
+#[pyo3(name = "split_tabix_by_barcode")]
+fn split_tabix_by_fragemnt_length_py(
+    tabix: String,
+    bins: HashMap<String, Vec<u64>>,
+    _n_threads: Option<u8>,
+)  -> PyResult<()> 
+{   
+
+    ctrlc::set_handler(|| std::process::exit(2)).unwrap_or_default();
+    let stats = split_tabix::split_tabix_by_fragment_size(&tabix, &bins).unwrap();
+    println!("{:?}", stats);
+
+    Ok(())
+
+}
+
 #[pymodule]
 fn rust_tsv_tools(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(split_tabix_py, m)?)?;
+    m.add_function(wrap_pyfunction!(split_tabix_by_barcode_py, m)?)?;
+    m.add_function(wrap_pyfunction!(split_tabix_by_fragemnt_length_py, m)?)?;
     Ok(())
 }
